e454ebd1c2d8b207b236f98c11e85f017f621399
bronx999-gif <channelgreenplay@gmail.com>
2025-11-18 21:37:45 +0530

    Create info.js

---
+++ api/info.js
@@ -0,0 +1,48 @@
+export default function handler(req, res) {
+  const { num, key } = req.query;
+
+  if (!key || key !== "jhat-ke-pakode") {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid API key"
+    });
+  }
+
+  if (!num) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide ?num="
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    result: [
+      {
+        id: 1205627021,
+        mobile: "7676184626",
+        name: "Shivalingappa",
+        father_name: "Durugappa",
+        address:
+          "S/O: Durugappa!257!!harijana colony!nadanga!Nadanga!Bellary!Karnataka!583114",
+        alt_mobile: "",
+        circle: "AIRTEL KARNATKA",
+        id_number: "",
+        email: ""
+      },
+      {
+        id: 1205627022,
+        mobile: "7676184626",
+        name: "Shivalingappa",
+        father_name: "Durugappa",
+        address:
+          "S/O Durugappa!!257 harijana colony!nadanga!Nadanga Bellary!Karnataka!583114",
+        alt_mobile: "919901542381",
+        circle: "KARNATAKA JIO",
+        id_number: "",
+        email: ""
+      }
+    ],
+    credit: "@BRONX_ULTRA"
+  });
+}
